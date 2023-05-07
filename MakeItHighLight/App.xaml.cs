dfbf1569e7ebdb1aed82cadc26276456c8c9b606
Marc Müller <marc.mueller@LIVE.AT>
2023-05-07 23:57:50 +0200

    Initial Class Structure :        - View Models               - Dependency Injections        - Views        - Communicator        - CommandsBase -App.cs    - Dependency Injections

--- MakeItHighLight/App.xaml.cs
+++ MakeItHighLight/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MakeItHighLight.Communicator;
+using MakeItHighLight.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,6 +15,26 @@ namespace MakeItHighLight
     /// </summary>
     public partial class App : Application
     {
-       
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+
+            Communicater communicater = new Communicater();
+
+
+            OverviewViewModel overviewViewModel = new OverviewViewModel(communicater);
+            SettingsViewModel settingsViewModel = new SettingsViewModel(communicater);
+            ImportViewModel importViewModel = new ImportViewModel(communicater);
+            HelpAndAboutViewModel helpViewModel = new HelpAndAboutViewModel(communicater);
+            StartupViewModel startupViewModel = new StartupViewModel(communicater);
+          
+            MainViewModel mainViewModel = new MainViewModel(communicater,overviewViewModel, settingsViewModel, importViewModel, helpViewModel, startupViewModel);
+
+            MainView MainView = new MainView()
+            {
+                DataContext = mainViewModel
+            };
+
+            MainView.Show();
+        }
     }
 }
---
+++ MakeItHighLight/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MakeItHighLight.Communicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeItHighLight.ViewModels
+{
+    internal class MainViewModel
+    {
+        private readonly Communicater _communicator;
+
+
+
+        public OverviewViewModel OverviewViewModel { get; }
+        public SettingsViewModel SettingsViewModel { get; }
+        public ImportViewModel ImportViewModel { get; }
+        public HelpAndAboutViewModel HelpAndAboutViewModel { get; }
+        public StartupViewModel Startup { get; }
+
+
+
+
+
+        public MainViewModel(Communicater communicator,
+                             OverviewViewModel overviewViewModel,
+                             SettingsViewModel settingsViewModel,
+                             ImportViewModel importViewModel,
+                             HelpAndAboutViewModel helpAndAboutViewModel,
+                             StartupViewModel startupViewModel)
+        {
+
+            _communicator= communicator;
+            OverviewViewModel = overviewViewModel;
+            SettingsViewModel = settingsViewModel;
+            ImportViewModel = importViewModel;
+            HelpAndAboutViewModel = helpAndAboutViewModel;          
+            Startup = startupViewModel;
+        }
+    }
+}
